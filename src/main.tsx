438add7b23ea184bc4d3620c01d6c3afbb750b02
ummesalma303 <ummesalma333999@gmail.com>
2025-05-12 17:37:44 +0600

    feat: basic setup

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css'
+// import App from './App.tsx'
+import Home from './page/Home.tsx';
+
+document.getElementById("root");
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+  </StrictMode>,
+)
---
+++ src/page/Home.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Welcome to My Home</h2>
+        <button className='text-red-500 bg-green-800'>Click Me</button>
+    </div>
+  )
+}
+
+export default Home
