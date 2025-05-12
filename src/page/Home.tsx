c2b989412d848ba06631d49a5891bcf20209bb31
ummesalma303 <ummesalma333999@gmail.com>
2025-05-12 18:14:50 +0600

    test: button component

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,10 +1,14 @@
 // import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
     <div>
         <h2>Welcome to My Home</h2>
-        <button className='text-red-500 bg-green-800'>Click Me</button>
+          <Button variant="destructive">Destructive</Button>
+        <Button className='text-red-500 bg-green-800'>Click Me</Button>
+        <Button>Secondary</Button>
+        <Button variant="secondary">Secondary</Button>
     </div>
   )
 }
